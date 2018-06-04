4cd7f25dda40259e9958daaba983f51eb20fc904
daiki-ypshimaru <godeater1104@yahoo.co.jp>
2018-06-04 23:13:01 +0900

    Pinballプロジェクト　
    Pinball作成と課題の提出です。

---
+++ Assets/ScorePoint.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ScorePoint : MonoBehaviour {
+    private GameObject Points;
+    int score = 0;
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "SmallStarTag") { score += 10; }
+        else if (collision.gameObject.tag == "LargeStarTag") { score += 20; }
+        else if (collision.gameObject.tag == "SmallCloudTag") { score += 40; }
+        else if (collision.gameObject.tag == "LargeCloudTag") { score += 50; }
+
+        Points.GetComponent<Text>().text = score.ToString();
+    }
+
+    // Use this for initialization
+    void Start () {
+        this.Points = GameObject.Find("Points");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
